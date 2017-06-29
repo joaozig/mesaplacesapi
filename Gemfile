dd288aa54776acdc530a50a1f8a1b899d29d46b1
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-06-29 18:19:49 -0300

    Add autenticação via devise_token_auth

--- Gemfile
+++ Gemfile
@@ -3,4 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.8'
 gem 'rails-api'
 gem 'pg'
+gem 'devise_token_auth'
+gem 'omniauth'
+gem 'rack-cors'
 gem 'spring', :group => :development
--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,16 @@ module Mesaplaces
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Setup CORS
+    config.middleware.use Rack::Cors do
+     allow do
+       origins '*'
+       resource '*',
+         :headers => :any,
+         :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+         :methods => [:get, :post, :options, :delete, :put]
+     end
+    end
   end
 end
