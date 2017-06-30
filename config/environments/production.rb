11876dbb468abe6ab5fac1a0bfa3fb8ad02d76b2
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-06-30 13:06:17 -0300

    Add configuração de email em ambiente produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,20 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Email configuration
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['SMTP_EMAIL'],
+    :password => ENV['SMTP_PASSWORD'],
+    :authentication => 'plain',
+    :enable_starttls_auto => true
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
